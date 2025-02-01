2c918e691c7a498a5244ff7f666ee2fdcc29b3c4
Cao Qihao <caoqihao88@gmail.com>
2025-02-01 17:56:34 +0800

    跟新环境依赖文件

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from mysite import views
 
 urlpatterns = [
     path("", views.index),
+    path('news/',include('news.urls')),
     path("admin/", admin.site.urls),
 ]
 
--- news/views.py
+++ news/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from news.models import News
+
 
 # Create your views here.
+def news_list(request):
+    news_list = News.objects.all()
+    print(news_list)
+    return render(request, "list.html", {"news_list": news_list})
+
+
+def detail(request, id):
+    try:
+        news = News.objects.get(pk=id)
+        return render(request, "detail.html", {"news": news})
+    except:
+        return render(request, "404.html")
